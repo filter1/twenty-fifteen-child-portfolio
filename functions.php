2a16055e13b05dde01ccd9722c94359bc632d6d5
Johannes Filter <hi@jfilter.de>
2017-02-07 14:01:28 +0100

    clean code

--- functions.php
+++ functions.php
@@ -10,29 +10,27 @@ function my_theme_enqueue_styles() {
         wp_get_theme()->get('Version')
     );
 }
+
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
-
 function wpb_list_child_pages() {
+	global $post;
 
-global $post;
+	if ( is_page() && $post->post_parent )
+		$childpages = get_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->post_parent . '&echo=0' );
+	else
+		$childpages = get_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->ID . '&echo=0' );
 
-if ( is_page() && $post->post_parent )
-	$childpages = get_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->post_parent . '&echo=0' );
-else
-	$childpages = get_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->ID . '&echo=0' );
+	if ( $childpages ) {
 
-if ( $childpages ) {
-
-	$string = "<div class='filter-container'>";
-	foreach($childpages as $page){
-		$string .= '<div>' . get_the_post_thumbnail( $page->ID, 'full') . '<p>' . get_the_title( $page->ID ) . '</p>' . '</div>';
+		$string = "<div class='filter-container'>";
+		foreach($childpages as $page){
+			$string .= '<div>' . get_the_post_thumbnail( $page->ID, 'full') . '<p>' . get_the_title( $page->ID ) . '</p>' . '</div>';
+		}
+		$string .= '</div>';
 	}
-	$string .= '</div>';
-}
 
-return $string;
-
+	return $string;
 }
 
 add_shortcode('wpb_childpages', 'wpb_list_child_pages');
@@ -40,6 +38,7 @@ add_shortcode('wpb_childpages', 'wpb_list_child_pages');
 function remove_oblique_footer_credits() {
 	remove_action( 'oblique_footer', 'oblique_footer_credits' );
 }
+
 add_action( 'get_footer', 'remove_oblique_footer_credits');
 ?>
 
